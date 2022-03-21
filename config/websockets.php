00a194008c255fe95088443e8d8f75473749f68d
Diego Chavez <ingdiegochavez18@gmail.com>
2022-03-21 10:37:15 -0500

    core/update-path

--- config/websockets.php
+++ config/websockets.php
@@ -59,7 +59,7 @@ return [
     /*
      * This path will be used to register the necessary routes for the package.
      */
-    'path' => 'laravel-websockets',
+    'path' => 'admin/laravel-websockets',
 
     /*
      * Dashboard Routes Middleware
